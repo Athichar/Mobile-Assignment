59d8578e1990cac07f9b2776a0d23ed69e8461ae
Athichar Kijcharoen <Kijcharoen_a@su.ac.th>
2025-10-18 12:03:08 +0700

    assignment-Week6

---
+++ lib/assignment.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  final String name;
+  final String position;
+  final String email;
+  final String phoneNumber;
+  final String imageUrl;
+
+  ProfileCard({
+    required this.name,
+    required this.position,
+    required this.email,
+    required this.phoneNumber,
+    required this.imageUrl,
+  });
+
+   @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(14),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(imageUrl),
+            ),
+            const SizedBox(height: 15),
+            Text(name, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            Text(position, style: const TextStyle(color: Colors.deepPurple)),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.email, size: 16, color: Colors.blue),
+                const SizedBox(width: 6),
+                Text(
+                  email,
+                  style: const TextStyle(color: Colors.blueGrey), 
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.phone, size: 16, color: Colors.green),
+                const SizedBox(width: 6),
+                Text(
+                  phoneNumber,
+                  style: const TextStyle(color: Colors.blueGrey),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
-import 'assignment2.dart';
-
+import 'assignment.dart'; 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Air Quality App',
-      debugShowCheckedModeBanner: false,
+      title: 'Custom Widget ProfileCard',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const AirQualityPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Widget ProfileCard'),
+        ),
+        body: Center(
+          child: FadeIn(
+            duration: Duration(seconds: 2), 
+            child: ProfileCard(
+              name: 'Athichar Kijcharoen',
+              position: 'Student',
+              email: 'Kijcharoen_a@su.ac.th',
+              phoneNumber: '0959943335',
+              imageUrl: 'https://scontent.fbkk7-3.fna.fbcdn.net/v/t39.30808-6/482349494_635153712549857_2279833133083138606_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeF5V750H3_7C9FjDa_WKVWER0U9EQMjYZFHRT0RAyNhkdxQX9YOrSiAAj3tgAPqsYJJF9n2GbPD__YZNA9-pogk&_nc_ohc=UAkuoDKEmdsQ7kNvwEG5yxL&_nc_oc=AdnEub9prAAEedistJ5QIDHmVEAd0FOIjSa80LelFKXKBNyXYmFP4SEZ2byE8_TA7tw&_nc_zt=23&_nc_ht=scontent.fbkk7-3.fna&_nc_gid=sv7H8JSxlWEP1MBL1cFJ9A&oh=00_AffbHeShPk9zuQrx3EjadHYGcPs3DMdfe10-UylNOrGuXw&oe=68F8D741', 
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FadeIn extends StatelessWidget {
+  final Widget child;
+  final Duration duration;
+
+  FadeIn({required this.child, required this.duration});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      opacity: 1.0,
+      duration: duration,
+      child: child,
     );
   }
 }
